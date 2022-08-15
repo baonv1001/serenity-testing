2efaf9f462a7ef02d34930568859fc7b090ee0b4
13a0_pr0_9x <baonv1001@gmail.com>
2022-08-15 11:28:41 +0700

    extends UIInteractionSteps

--- src/test/java/serenityswag/WhenLoggingOn.java
+++ src/test/java/serenityswag/WhenLoggingOn.java
@@ -1,5 +1,6 @@
 package serenityswag;
 
+import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import org.junit.Test;
@@ -8,15 +9,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 @RunWith(SerenityRunner.class)
-public class WhenLoggingOn {
+public class WhenLoggingOn extends UIInteractionSteps {
     @Managed
     WebDriver driver;
 
     @Test
     public void usersCanLogOnViaTheHomePage() {
         driver.get("https://www.facebook.com/");
-        driver.findElement(By.cssSelector("#email")).sendKeys("acquyhexom1@gmail.com");
+        openUrl("https://www.facebook.com/");
+        /*driver.findElement(By.cssSelector("#email")).sendKeys("acquyhexom1@gmail.com");
         driver.findElement(By.cssSelector("#pass")).sendKeys("NiflFaejY92");
-        driver.findElement(By.cssSelector("[data-testid='royal_login_button']")).click();
+        driver.findElement(By.cssSelector("[data-testid='royal_login_button']")).click();*/
+
+        find("#email").sendKeys("acquyhexom1@gmail.com");
+        find("#pass").sendKeys("NiflFaejY92");
+        find("[data-testid='royal_login_button']").click();
     }
 }
